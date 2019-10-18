2a6b68b0f773732dea1c4b1c0a69d711bef7dfcd
supercmmetry <vishaals2000@gmail.com>
2019-10-18 17:25:34 +0530

    Add I/O based process addition

--- main.cpp
+++ main.cpp
@@ -4,16 +4,19 @@
 int main()
 {
     Scheduler sh(100);
-    sh.alpha = 7;
+    sh.alpha = 7; // context-switching loss: 12.5%
     sh.setContextSwitchingTime(1);
 
-    int N = 5;
+    int N = 0;
+    std::cout << "Enter the number of processes: " << std::endl;
+    std::cin >> N;
     proc_t* procs = new proc_t[N];
-    procs[0] = proc_t{0, 1, 4, 3};
-    procs[1] = proc_t{1, 0, 2, 2};
-    procs[2] = proc_t{2, 2, 3, 3};
-    procs[3] = proc_t{3, 20, 4, 50};
-    procs[4] = proc_t{4, 0, 9, 7};
+
+    for(int i = 0; i < N; i++) {
+        std::cout << "Enter process (id, arrival time, burst time, priority)" << std::endl;
+        std::cin >> procs[i].id >> procs[i].ar_time >> procs[i].burst_time >> procs[i].priority;
+    }
+
     sh.loadProcesses(procs, N);
     sh.run();
     std::cout << "Run completed.." << std::endl;
